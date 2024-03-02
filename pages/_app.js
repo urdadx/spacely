8b3b786d13ef7a1fa9fb5f6266a4573c6d59fc14
urdad <70736338+urdadx@users.noreply.github.com>
2024-03-02 16:52:13 +0000

    added vercel analytics✨

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '@/styles/globals.css';
 import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Toaster position="bottom-center" />
+      <Analytics />
     </>
   );
 }
